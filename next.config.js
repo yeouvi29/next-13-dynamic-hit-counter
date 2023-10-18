22c3d55e80db521c5d3d019b407115a0401ad6cf
emily <yeouvi29@gmail.com>
2023-10-17 17:49:06 -0700

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,7 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-const withImages = require("next-images");
-const withCSS = require("@zeit/next-css");
-
-module.exports = withBundleAnalyzer(
-  withImages(
-    withCSS({
-      // 여기에 기존의 Next.js 설정을 추가합니다.
-
-      webpack: (
-        config,
-        { buildId, dev, isServer, defaultLoaders, webpack }
-      ) => {
-        // JSON 파일을 포함시키기 위한 설정
-        config.module.rules.push({
-          test: /\.json$/,
-          type: "javascript/auto",
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "static/chunks/database.json",
-              },
-            },
-          ],
-        });
-
-        return config;
-      },
-    })
-  )
-);
+module.exports = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./src/database.json"],
+    },
+  },
+};
